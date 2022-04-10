4d8dce69a17b23e891dac93f681993757b6cdcf3
derickfan <fanderick9999@gmail.com>
2022-04-10 23:37:35 +0200

    lsp-metals key mapping

--- lua/user/lsp-metals.lua
+++ lua/user/lsp-metals.lua
@@ -1,13 +1,43 @@
 local metals_config = require("metals").bare_config()
 
 metals_config.settings = {
-  showImplicitArguments = true
+  showImplicitArguments = true,
+  excludedPackages = {},
 }
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+metals_config.capabilities = capabilities
 
 vim.cmd [[augroup lsp]]
 vim.cmd [[autocmd!]]
 vim.cmd [[autocmd FileType scala setlocal omnifunc=v:lua.vim.lsp.omnifunc]]
 vim.cmd [[au FileType java,scala,sbt lua require("metals").initialize_or_attach({metals_config})]]
+vim.cmd [[autocmd FileType scala lua SetMetalsKeymap()]]
 vim.cmd [[augroup end]]
 
 
+function SetMetalsKeymap()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+  -- vim.api.nvim_buf_set_keymap(
+  --   bufnr,
+  --   "n",
+  --   "gl",
+  --   '<cmd>lua vim.lsp.diagnostic.open_float()<CR>',
+  --   opts
+  -- )
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
